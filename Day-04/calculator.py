d7ec93d0ce1b00cd5c0d7c1df5b8e3cff772c2bf
sushantkapare1717@gmail.com <sushant@13.com>
2023-11-03 10:56:43 +0530

    added calculator.py file

---
+++ Day-04/calculator.py
@@ -0,0 +1,25 @@
+num1 = 10
+num2 = 5
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def substraction():
+    sub = num1 - num2
+    print(sub)
+
+def multiplication():
+    mul = num1 * num2
+    print(mul)
+
+
+def division():
+    div = num1 / num2
+    print(div)
+
+
+addition()
+substraction()
+multiplication()
+division()
