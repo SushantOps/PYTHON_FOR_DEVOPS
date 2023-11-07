5eca4fed3719698cad2cad96cb1ce28b54e7d48b
sushantkapare1717@gmail.com <sushant@13.com>
2023-11-07 18:23:18 +0530

    added day-08

--- Day-08/01_list_test.py
+++ Day-08/01_list_test.py
@@ -2,3 +2,10 @@
 
 s3_bucket_list = ["sush_bucket", "amar_bucket", "hemant_bucket"]
 print(s3_bucket_list)
+
+
+
+## List Indexing
+
+s3_bucket_list = ["sush_bucket", "amar_bucket", "hemant_bucket"]
+print(s3_bucket_list[0])
