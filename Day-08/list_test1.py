03c2e4da71104804b33e1612d17bd482ab6da55f
sushantkapare1717@gmail.com <sushant@13.com>
2023-11-07 18:15:17 +0530

    added new files of day-08

---
+++ Day-08/list_test1.py
@@ -0,0 +1,6 @@
+## added new element to list
+
+s3_bucket_list = ["sush_bucket", "amar_bucket", "hemant_bucket"]
+s3_bucket_list.append("new_s3_bucket")
+
+print(s3_bucket_list)
---
+++ Day-08/tuple_test.py
@@ -0,0 +1,4 @@
+##Tuple
+
+s3_bucket_list = ("sush_bucket", "amar_bucket", "hemant_bucket")
+print(type(s3_bucket_list))
