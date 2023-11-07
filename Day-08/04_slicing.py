2d6951cf7ba7a0ea29bb7bbc3445efbc59ad7f67
sushantkapare1717@gmail.com <sushant@13.com>
2023-11-07 18:41:35 +0530

    added day-08

---
+++ Day-08/04_slicing.py
@@ -0,0 +1,6 @@
+## Slicing
+
+s3_bucket_list = ["sush_bucket", "amar_bucket", "hemant_bucket"]
+
+new_list = s3_bucket_list[0:2]
+print(new_list)
---
+++ Day-08/05_hetrogenious_list.py
@@ -0,0 +1,5 @@
+## hetrogenious list (In list every data type considered as a element)
+
+
+random_list = [2,6,9,"sush","amar", 6.7, 8.99]
+print(random_list)
