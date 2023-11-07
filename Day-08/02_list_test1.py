6f8ddbb55f5c529da534d6cef6febb2c3924403a
sushantkapare1717@gmail.com <sushant@13.com>
2023-11-07 18:18:58 +0530

    modify day-08

--- Day-08/02_list_test1.py
+++ Day-08/02_list_test1.py
@@ -4,3 +4,12 @@ s3_bucket_list = ["sush_bucket", "amar_bucket", "hemant_bucket"]
 s3_bucket_list.append("new_s3_bucket")
 
 print(s3_bucket_list)
+
+
+
+## remove new element to list
+
+s3_bucket_list = ["sush_bucket", "amar_bucket", "hemant_bucket"]
+s3_bucket_list.remove("new_s3_bucket")
+
+print(s3_bucket_list)
