f659371583d950ff81a187f022b348cef4057c22
sushantkapare1717@gmail.com <sushant@13.com>
2023-11-22 09:55:46 +0530

    added test2.py

---
+++ Day-11/test2.py
@@ -0,0 +1,21 @@
+## List of Dictionaries
+
+ec2_instance_info = [
+    {
+        "Id": "Instance_id-001",
+        "type": "t2.small"
+    },
+
+    {
+        "Id": "Instance_id-002",
+        "type": "t2.medium"
+    },
+
+    {
+        "Id": "Instance_id-003",
+        "type": "t2.xlarge"
+    }
+
+]
+
+print(ec2_instance_info[1]["type"])
